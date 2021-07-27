2c739a725498e0e7e3588c3e64a1d414ba5ed658
Jeeva R <jeevanadar1998@gmail.com>
2021-07-27 23:45:17 +0530

    prime number java file is added

---
+++ AcheronAssignment/src/com/shrishti/training/number/PrimeNumber.java
@@ -0,0 +1,30 @@
+package com.shrishti.training.number;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+	static boolean isPrime(int number) {
+		for(int value=2;value<number;value+=2) {
+			if(number%value==0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter the number: ");
+		int number=scanner.nextInt();
+		if(isPrime(number)) {
+			System.out.println(number+" is a prime number");
+		}
+		else {
+			System.out.println(number+" is not a prime number");
+
+		}
+
+	}
+
+}
