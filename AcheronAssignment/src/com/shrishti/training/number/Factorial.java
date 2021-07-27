b05296e6b12ef06dc17828d29c0304df31cdfe31
Jeeva R <jeevanadar1998@gmail.com>
2021-07-27 21:23:15 +0530

    factorial java file is added

---
+++ AcheronAssignment/src/com/shrishti/training/number/Factorial.java
@@ -0,0 +1,17 @@
+package com.shrishti.training.number;
+import java.util.Scanner;
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter the number: ");
+		int number=scanner.nextInt();
+		int factorial=1;
+		while(number>1) {
+			factorial*=number--;
+		}
+		System.out.println("The factorial of a "+number+" is "+factorial);
+	}
+
+}
